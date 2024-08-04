ef6c3d3707207d6c9547f58e73287b623f4976dc
Jodyssey <jaraenpiwat39@hotmail.com>
2024-08-04 22:25:18 +0700

    CORS

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
+const corsOptions = {
+  origin: ['http://example.com',
+    'http://localhost:4200',
+    'https://www.w3schools.com'],
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use('/image', express.static('./images'))
 app.use(express.json());
 
 app.use(require('./src/routes/routes'))
